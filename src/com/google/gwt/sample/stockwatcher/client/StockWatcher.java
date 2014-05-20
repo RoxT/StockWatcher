25648935896c15ca589471fa2f0b5d73358afed2
Angela Chen <angelaichen87@gmail.com>
2014-05-19 17:27:21 -0700

    Fixed random bug. List no longer refreshes at stated interval.

--- src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
+++ src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -121,7 +121,7 @@ public class StockWatcher implements EntryPoint {
 			}
 
 		};
-		refreshTimer.scheduleRepeating(REFRESH_INTERVAL);
+
 
 
 		// Listen for mouse events on the Add button.
@@ -150,8 +150,9 @@ public class StockWatcher implements EntryPoint {
 
 		StockPrice[] prices = new StockPrice[stocks.size()];
 		for (int i = 0; i < stocks.size(); i++) {
-			double price = 100;
-			double change = .2;
+			double price = Random.nextDouble() * MAX_PRICE;
+			double change = price * MAX_PRICE_CHANGE
+					* (Random.nextDouble() * 2.0 - 1.0);
 
 			prices[i] = new StockPrice(stocks.get(i), price, change);
 		}
